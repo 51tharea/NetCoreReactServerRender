bf33e01539c75e134a80f264fdcce5394398de9c
Can Acar <can.acar@windowslive.com>
2019-09-13 20:30:36 +0300

    # some changes #

---
+++ NetCoreReactServerRender/ClientApp/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Application = () => {
+    return <div> .Net Core Server Render with Hot Module Load. </div>
+}
+export {Application};
--- NetCoreReactServerRender/webpack.config.js
+++ NetCoreReactServerRender/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require("path");
+const webpack = require('webpack');
 const merge = require("webpack-merge");
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const CssNanoPlugin = require("cssnano");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = env => {
     const isDevBuild = !(env && env.prod);
 
@@ -11,7 +17,7 @@ module.exports = env => {
             minimize: !isDevBuild,
             usedExports: isDevBuild
         },
-        stats: { modules: false },
+        stats: {modules: false},
         resolve: {
             extensions: [".js", ".jsx", ".json", ".jpg", ".css"]
         },
@@ -24,9 +30,10 @@ module.exports = env => {
                 {
                     test: /\.js(x?)$/,
                     include: /ClientApp/,
+                    exclude: /node_modules/,
                     use: [
                         {
-                            loader: "babel-loader"
+                            loader: "babel-loader",
                         }
                     ]
                 },
@@ -37,30 +44,92 @@ module.exports = env => {
                 {
                     test: /\.css$/,
                     use: ["style-loader", "css-loader"]
+                },
+                {
+                    // Match woff2 in addition to patterns like .woff?v=1.1.1.
+                    test: /\.(woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "file-loader",
+                    options: {
+                        limit: 50000,
+                        name: "./fonts/[name].[ext]"
+                    }
                 }
             ]
-        }
+        },
+        plugins: [
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+        ].concat(isDevBuild ? [
+            // Development.
+
+            // Add module names to factory functions so they appear in browser profiler.
+            new webpack.NamedModulesPlugin(),
+            // Watcher doesn't work well if you mistype casing in a path so we use
+            // a plugin that prints an error when you attempt to do this.
+            // See https://github.com/facebookincubator/create-react-app/issues/240
+            new CaseSensitivePathsPlugin(),
+            // If you require a missing module and then `npm install` it, you still have
+            // to restart the development server for Webpack to discover it. This plugin
+            // makes the discovery automatic so you don't have to restart.
+            // See https://github.com/facebookincubator/create-react-app/issues/186
+            new WatchMissingNodeModulesPlugin(path.resolve(__dirname, '..', 'node_modules'))
+        ] : [
+            // Production.
+        ])
     };
 
     if (isDevBuild) {
         // Change config for development build.
         sharedConfig = {
             ...sharedConfig,
-            devtool: "eval-source-map"
+            devtool: isDevBuild
+                ? '#eval-source-map'
+                : 'source-map',
+
         };
     }
 
     // Configuration for client-side bundle suitable for running in browsers.
     const clientBundleOutputDir = "./wwwroot/dist";
     const clientBundleConfig = merge(sharedConfig, {
-        entry: { "main-client": "./ClientApp/boot-client.js" },
-        output: { path: path.join(__dirname, clientBundleOutputDir) }
+        entry: {"bundle": "./ClientApp/boot-client.js"},
+        module: {
+            rules: [
+                {test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : [MiniCssExtractPlugin.loader, 'css-loader']},
+                {test: /\.(scss|sass)$/, use: isDevBuild ? ['style-loader', 'css-loader', 'sass-loader'] : [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']}
+            ]
+        },
+        output: {path: path.join(__dirname, clientBundleOutputDir)},
+        node: {
+            dgram: 'empty',
+            fs: 'empty',
+            net: 'empty',
+            tls: 'empty',
+            child_process: 'empty',
+        },
+        plugins: [].concat(isDevBuild ? [
+            // Development.
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps.
+                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+            // Production.
+            new MiniCssExtractPlugin({
+                filename: "site.css"
+            })
+        ])
     });
 
     // Configuration for server-side (prerendering) bundle suitable for running in Node.
     const serverBundleConfig = merge(sharedConfig, {
-        resolve: { mainFields: ["main"] },
-        entry: { "main-server": "./ClientApp/boot-server.js" },
+        module: {
+            rules: [
+                {test: /\.(scss|sass)$/, use: "ignore-loader"}
+            ]
+        },
+        resolve: {mainFields: ["main"]},
+        entry: {"main-server": "./ClientApp/boot-server.js"},
+        plugins: [],
         output: {
             libraryTarget: "commonjs",
             path: path.join(__dirname, "./ClientApp/dist")
