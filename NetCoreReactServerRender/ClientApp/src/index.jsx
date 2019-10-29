85b2d566f9c7e2662eef277b51574a73ad99721c
Can Acar <can.acar@windowslive.com>
2019-10-29 15:48:49 +0300

    ..

--- NetCoreReactServerRender/ClientApp/src/index.jsx
+++ NetCoreReactServerRender/ClientApp/src/index.jsx
@@ -2,10 +2,7 @@ import React, {Fragment} from 'react';
 
 const Application = () => {
     return (
-        <Fragment>
-            <div> .Net Core 3.0 Server Render with Hot Module Loaded... </div>
-            <p> Razor Page </p>
-        </Fragment>
+       <
         
     )
 }
